3c07accc8debf66cb910a9732ddf0262390946fe
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-09-03 09:40:32 +0500

    adding Functionality for Image Uploader in UserAdminPannel (009)

--- FirebaseConfig/firebase.js
+++ FirebaseConfig/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyDxkQUaot3SHRIJTz4_hrgTo_wgB0oaaqE",
   authDomain: "todo-backend-bcd23.firebaseapp.com",
@@ -17,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export {analytics, db , auth };
+export { analytics, db, auth , storage };
--- src/components/ImageUploader/ImageUploader.jsx
+++ src/components/ImageUploader/ImageUploader.jsx
@@ -1,10 +1,32 @@
+import { useRef} from "react";
 import "./ImageUploader.css";
-
-function ImageUploader()
- {
-    const removeImageFileUploader = ()=>{
-        document.getElementById("image-file-uploader").style.display = "none"
+import { ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../../FirebaseConfig/firebase";
+function ImageUploader() {
+  const fileUploader = async (storageFolderName, file) => {
+    try {
+      const storageRef = ref(storage, `${storageFolderName}`);
+      const snapshot = await uploadBytes(storageRef, `${file}`);
+      console.log("Uploaded a blob or file===>" , snapshot );
+      
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  const removeImageFileUploader = () => {
+    document.getElementById("image-file-uploader").style.display = "none";
+  };
+
+  const image = useRef();
+
+  const getUploadedFile = (evt) => {
+    evt.preventDefault();
+    console.log(image.current);
+   const uploadedImage = image.current.files[0];
+   console.log(uploadedImage);
+    fileUploader(`userImages/${uploadedImage.name}` , uploadedImage);
+  };
   return (
     <>
       <div
@@ -12,16 +34,21 @@ function ImageUploader()
         className="flex flex-col w-[400px] h-[fit-content] px-3 py-1 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
       >
         <div
-        onClick={removeImageFileUploader}
+          onClick={removeImageFileUploader}
           className="self-end text-[24px] text-bold hover:text-[red] cursor-pointer"
         >
           <i className="fa-solid fa-xmark"></i>
         </div>
-        <form className="flex flex-col items-center justify-center  p-6 gap-4">
+        <form
+          onSubmit={getUploadedFile}
+          className="flex flex-col items-center justify-center  p-6 gap-4"
+        >
           <input
             type="file"
+            accept="image/*"
             name="profile-image-input"
             id="profile-image-input-field"
+            ref={image}
           />
           <button
             type="submit"
